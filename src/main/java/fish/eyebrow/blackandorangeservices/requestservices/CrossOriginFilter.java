46f1b66669c2fdcadf23a93e77366f681f28fe71
Aliics <Aliics@hotmail.com>
2018-10-22 10:31:01 +1300

    Updated Access-Control-Allow-Headers to match possible requests.

--- src/main/java/fish/eyebrow/blackandorangeservices/requestservices/CrossOriginFilter.java
+++ src/main/java/fish/eyebrow/blackandorangeservices/requestservices/CrossOriginFilter.java
@@ -10,7 +10,11 @@ public class CrossOriginFilter implements ContainerResponseFilter {
 	@Override
 	public void filter(ContainerRequestContext request, ContainerResponseContext response) {
 		final String requestOrigin = request.getHeaders().getFirst("Origin");
+		final String[] allowedHeaders = {
+				"Content-Type"
+		};
 
 		response.getHeaders().add("Access-Control-Allow-Origin", requestOrigin);
+		response.getHeaders().add("Access-Control-Allow-Headers", String.join(", ", allowedHeaders));
 	}
 }
