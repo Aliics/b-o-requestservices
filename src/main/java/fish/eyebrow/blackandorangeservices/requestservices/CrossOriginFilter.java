aae584e2a4997f5cfb59dd0206283d95551aa17e
Aliics <Aliics@hotmail.com>
2018-10-22 10:24:16 +1300

    Shortened CrossOriginFilter to only contain NECESSARY information.

--- src/main/java/fish/eyebrow/blackandorangeservices/requestservices/CrossOriginFilter.java
+++ src/main/java/fish/eyebrow/blackandorangeservices/requestservices/CrossOriginFilter.java
@@ -7,19 +7,10 @@ import javax.ws.rs.ext.Provider;
 
 @Provider
 public class CrossOriginFilter implements ContainerResponseFilter {
-
 	@Override
-	public void filter(ContainerRequestContext request,
-	                   ContainerResponseContext response) {
-		response.getHeaders().add("Access-Control-Allow-Origin", "*");
-		response.getHeaders().add(
-				"Access-Control-Allow-Headers",
-				"origin, content-type, accept, authorization"
-		);
-		response.getHeaders().add("Access-Control-Allow-Credentials", "true");
-		response.getHeaders().add(
-				"Access-Control-Allow-Methods",
-				"GET, POST, PUT, DELETE, OPTIONS, HEAD"
-		);
+	public void filter(ContainerRequestContext request, ContainerResponseContext response) {
+		final String requestOrigin = request.getHeaders().getFirst("Origin");
+
+		response.getHeaders().add("Access-Control-Allow-Origin", requestOrigin);
 	}
 }
